3c56fa78eacfa176b345796e92fcf0f25bedfcf7
Benjamin <bstanf@uw.edu>
2022-11-19 20:23:30 -0800

    forgot to separate new and InjectManifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = () => {
         title: 'JATE'
       }),
       // Service Worker
-      newInjectManifest({
+      new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js'
       }),
