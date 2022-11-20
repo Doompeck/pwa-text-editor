76edf809e52e610967cc9c4643ad5ca615b9e4da
Benjamin <bstanf@uw.edu>
2022-11-19 21:09:31 -0800

    putDb function is built and commented

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,22 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+// create the database connection
+  const dbComm = await openDB('jate', 1);
+// create a new database transaction and define it's privileges
+  const transact = dbComm.transaction('jate', 'readwrite');
+// open the object store
+  const store = transact.objectStore('jate');
+// pass content into the store
+  const req = store.put({ id: 1, value: content });
+// confirm the request
+  const result = await req;
+
+  console.log('data has been stored', result);
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
